8b7b3d1502e2a599399dd5889c485f6ae02df65b
Adam Helf <adam-helf@uiowa.edu>
2019-11-05 14:32:39 -0600

    modified it so I am adding brands instead of friends

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='!QAZ1qaz'
+
+class BrandForm(FlaskForm):
+    name = StringField('Brand Name:', validators=[DataRequired()])
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', pageTitle='Adam\'s Brands')
+
+@app.route('/add_brand', methods=['GET', 'POST'])
+def add_brand():
+    form =BrandForm()
+    if form.validate_on_submit():
+        return "my Brands name is {0}".format(form.name.data)
+    
+    return render_template('add_brand.html', form=form, pageTitle='Add A New Brand')    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
